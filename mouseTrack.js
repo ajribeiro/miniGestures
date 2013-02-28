e0f08eb075d8a59d1d2a6f615ebcc834d9ecc82a
AJ Ribeiro <ajribeiro86@gmail.com>
2013-02-27 23:00:02 -0500

    tracing of mouse works

--- mouseTrack.js
+++ mouseTrack.js
@@ -1,18 +1,51 @@
 var mousedown = false
-var mx,my,nx,ny,count,phi
+var mx,my,nx,ny,lx,ly,count,phi
 var move="", omove, tmove
 var pi =3.14159
 var moved=true
+var canvas, canvasContainer
 
+function createCanvas()
+{
+      canvas = document.createElement('canvas');    
+	  canvas.style.width=document.width
+	  canvas.style.height=document.height
+		canvas.width=document.width
+		canvas.height=document.height	  
+		canvas.style.left="0px";
+		canvas.style.top="0px";
+      canvas.style.overflow = 'visible';
+      canvas.style.position = 'absolute';
+	  canvas.style.zIndex="1000"
+}
+function draw(x,y)
+{
+	var ctx = canvas.getContext('2d');
+	// alert('draw')
+	// alert(ctx)
+	// ctx.style.zIndex  = 1000"
+	ctx.beginPath();
+	ctx.strokeStyle="ff3300"
+	ctx.lineWidth=3
+	ctx.moveTo(lx,ly);
+	ctx.lineTo(x,y);
+	ctx.stroke()
+    lx=x
+	ly=y
+}
 document.onmousedown = function(event)
 {
 	//right mouse click
 	if(event.which == 3 && moved)
 	{
+		createCanvas()
+		document.body.appendChild(canvas);
 		moved=true
 		count = 0
 		my = event.pageX;
 		mx = event.pageY;
+		lx = my
+		ly = mx
 		mousedown = true
 	}
 };
@@ -22,6 +55,8 @@ document.onmouseup = function(event)
 	//right mouse click
 	if(event.which == 3)
 	{
+		document.body.removeChild(canvas)
+		canvas.width = canvas.width;
 		mousedown = false
 		if(move != "")
 		{
@@ -43,6 +78,7 @@ document.onmousemove = function(event)
 	{
 		ny = event.pageX;
 		nx = event.pageY;
+		draw(ny,nx)
 		var r = Math.sqrt(Math.pow(nx-mx,2)+Math.pow(ny-my,2))
 		if(r > 9)
 		{
